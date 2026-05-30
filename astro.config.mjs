5dc72bb95614d3d35c37c9892d17d769b81c50f5
evan-gan <139721374+evan-gan@users.noreply.github.com>
2026-05-29 21:26:27 -0400

    Add guide!

--- astro.config.mjs
+++ astro.config.mjs
@@ -19,6 +19,7 @@ export default defineConfig({
 				{
 					label: 'Guides',
 					items: [
+						{ slug: 'guides/shipping-guide' },
 						{ slug: 'guides/git-guide' },
 						{ slug: 'guides/website-guide' },
 						{ slug: 'guides/godot-guide' },
